5c4a46f6a7ba5f40da6d40e59b6ddc972eaa23a6
ma7moud3ly <engma7moud3ly@gmail.com>
2020-08-19 06:03:54 +0000

    uplaod readme

---
+++ generate_readme.py
@@ -0,0 +1,16 @@
+books=eval(open('mybooks.json','r',encoding='utf-8').read())
+
+f=open('README.md','w+',encoding='utf-8')
+f.truncate(0)
+f.write('<div align="center">')
+f.write('<h4>All books in <a href="https://hindawi.org">hindawi.org</a></h4><br>')
+
+for i in range(len(books)):
+  book=books[i]
+  name=book['name']
+  id=book['id']
+  f.write('<a href="txts/%s.txt">%s</a><br>'%(id,name))
+
+f.write('</div>')
+f.close()
+print('done')
